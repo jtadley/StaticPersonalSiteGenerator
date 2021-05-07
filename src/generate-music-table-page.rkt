b52d8b608ce6d74f92ae7ec71c86f8406f9f9cde
jtadley <jtadley@protonmail.com>
2021-05-07 17:38:54 -0500

    music working fine; filtering by genres a little scuffed

--- src/generate-music-table-page.rkt
+++ src/generate-music-table-page.rkt
@@ -4,11 +4,12 @@
  racket/struct)
 
 (define DIR "../SiteSource/Music")
-(define TITLE "Music")
-(define BACK-PAGE "Music.html")
-(define HOME-PAGE "../index.html")
-(define OUT-FILE "out")
+(define TITLE "All Music")
+(define BACK-PAGE "index.html")
+(define HOME-PAGE "../../index.html")
+(define STYLE-SOURCE "../../styles.css")
 (define SEPARATOR ":")
+(define OUT-PREFIX "../out/")
 
 ;; -----
 
@@ -177,17 +178,21 @@
      "</table>")))
 
 (define build-html-table
-  (λ (dir)
+  (λ (dir filter-pred sort-fn)
     (let* ([files (get-all-source-files-from-dir dir)]
            [lines (map-source-files-to-lines files)]
            [data (map-file-lines-to-table-data lines)]
+           [data (if filter-pred (filter filter-pred data) data)]
+           [data (if sort-fn (sort data sort-fn) data)]
            [table (list-of-table-data->html-table data)])
       table)))
 
 (define get-html-header
   (λ (back-link home-link)
     (string-append
-     "<!DOCTYPE html><html><head><link rel=\"stylesheet\" href=\"styles.css\"></head><body><h5><span class=\"left\"><a href=\""
+     "<!DOCTYPE html><html><head><link rel=\"stylesheet\" href=\""
+     STYLE-SOURCE
+     "\"></head><body><h5><span class=\"left\"><a href=\""
      back-link
      "\">Back</a></span><span class=\"right\"><a href=\""
      home-link
@@ -205,16 +210,114 @@
      "</h1>")))
 
 (define build-html-table-page-music
-  (λ (dir title back-link home-link)
+  (λ (dir title back-link home-link filter-pred sort-fn)
     (string-append
      (get-html-header back-link home-link)
      (build-html-title title)
-     (build-html-table dir)
+     (build-html-table dir filter-pred sort-fn)
      (get-html-footer))))
 
 (define build
-  (λ ()
-    (let ([page-str (build-html-table-page-music DIR TITLE BACK-PAGE HOME-PAGE)])
-      (display-to-file page-str OUT-FILE))))
+  (λ (out-file filter-pred sort-fn)
+    (let ([page-str (build-html-table-page-music DIR TITLE BACK-PAGE HOME-PAGE filter-pred sort-fn)])
+      (display-to-file page-str out-file))))
 
-(build)
+(define build-all
+  (λ ()
+    (build "All.html" #f (λ (td1 td2)
+                           (< (string->number (table-data-rating td1)) (string->number (table-data-rating td2)))))))
+
+(define RATINGS (list 1 2 3 4 5 6 7 8 9 10))
+
+(define build-numbers
+  (λ (lon δ title accessor)
+    (let ([title-suffix (if (zero? δ)
+                            ""
+                            "s")])
+      (letrec ([helper (λ (ls)
+                        (if (not (empty? ls))
+                            (begin
+                              (build
+                               (string-append title "-" (number->string (car ls)) title-suffix ".html")
+                               (λ (td)
+                                 (let ([cur-num (string->number (accessor td))])
+                                   (if (zero? δ)
+                                       (equal? cur-num (car ls))
+                                       (and
+                                        (>= cur-num (car ls))
+                                        (< cur-num (+ (car ls) δ))))))
+                               (if (zero? δ)
+                                   #f
+                                   <))
+                              (helper (cdr ls)))
+                            (writeln (string-append "done-" title))))])
+        (helper lon)))))
+
+(define build-rating
+  (λ ()
+    (build-numbers RATINGS 0 "Rating" (λ (td) (table-data-rating td)))))
+
+(define YEARS (list 1960 1970 1980 1990 2000 2010 2020))
+
+(define build-year
+  (λ ()
+    (build-numbers YEARS 10 "Year" (λ (td) (table-data-year td)))))
+
+(define ALPHABET (list #\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L #\M #\N #\O #\P #\Q #\R #\S #\T #\U #\V #\W #\X #\Y #\Z))
+
+(define build-alphabet
+  (λ (title accessor)
+    (let* ([leading-char (λ (td) (string-ref (accessor td) 0))]
+           [sort-fn string<=?])
+      (letrec ([helper (λ (ls)
+                         (if (not (empty? ls))
+                             (begin
+                               (build
+                                (string-append title "-" (string (car ls)) ".html")
+                                (λ (td)
+                                  (equal? (car ls) (char-upcase (leading-char td))))
+                                sort-fn)
+                               (helper (cdr ls)))
+                             (begin
+                               (build
+                                (string-append title "-Other.html")
+                                (λ (td)
+                                  (not (char-alphabetic? (leading-char td))))
+                                sort-fn)
+                               (writeln (string-append "done-" title)))))])
+        (helper ALPHABET)))))
+
+(define build-artist
+  (λ ()
+    (build-alphabet "Artist" (λ (td) (table-data-artist td)))))
+
+(define build-album
+  (λ ()
+    (build-alphabet "Album" (λ (td) (table-data-album td)))))
+
+(define build-in-list
+  (λ (ls title accessor)
+    (if (empty? ls)
+        (writeln (string-append "done-" title))
+        (begin
+          (build
+           (string-append title "-" (car ls) ".html")
+           (λ (td) (string-contains? (accessor td) (car ls)))
+           (λ (td1 td2) (< (table-data-rating td1) (table-data-rating td2))))
+          (build-in-list (cdr ls) title accessor)))))
+
+(define GENRES (list "Shoegaze" "Noise" "Industrial" "Jazz" "Art"))
+
+(define build-genre
+  (λ ()
+    (build-in-list GENRES "Genre" (λ (td) (table-data-genres td)))))
+
+(define go
+  (λ ()
+    (build-all)
+    (build-rating)
+    (build-year)
+    (build-artist)
+    (build-album)
+    (build-genre)
+    ))
---
+++ src/move-html-to-server.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+scp *.html jacob@192.168.1.177:~/Site/Music
